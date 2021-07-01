cc4b723697ac4098e7cb061f16f55d19b7b47d32
Fernando Martinez <fmartinezr195@gmail.com>
2021-06-30 22:53:33 -0400

    [conf] Modelo de dominio y DTO
    En este commit se agregara el modelo de dominio donde se manejan los
    tipos e interfaces que manejaran las reglas de negocio. Ademas se
    agregara  un DTO para BasicJobOfferDTO para la interfaz de la oferta de
    trabajo.
    
    Se realizara con el fin de manejar el core del negocio, el modelo mas
    puro de la arquitectura.
    
    ID tarea: Subtarea de: E4.1.1 , E4.1.2 , E4.3.1

---
+++ src/job-offer/domain/Employer.ts
@@ -0,0 +1,25 @@
+import { User } from './User'
+import { Skill, Status } from './JobOffer'
+
+type Name = string
+type Postcod = number
+type Address = {
+  residentialInfo: Name
+  street: Name
+  city: Name
+  country: Name
+  postCod: Postcod
+}
+
+//URL TO IMAGE
+type Logo = string
+
+export interface Employer {
+  companyName: Name
+  address: Address
+  contacts: User[]
+  logo: Logo
+  skills: Skill[]
+  specialRequeriments: Name
+  status: Status
+}
