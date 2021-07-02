6d224c8ca0d1bc008da4b3d30780ee14fcb2672f
Daniel Rivero <danielrivero2000@gmail.com>
2021-07-01 22:24:12 -0400

    skladjakld

--- src/job-offer/infrastructure/driven-adapters/in/http/GetOfferAdapter.ts
+++ src/job-offer/infrastructure/driven-adapters/in/http/GetOfferAdapter.ts
@@ -6,13 +6,15 @@ export class Http implements GetOfferPort {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8081',
       },
+      credentials: 'same-origin',
     })
     return (await response.json()) as T
   }
 
   async requestHandler(id: any): Promise<any> {
-    const response = await this.fetchData<any>('http://localhost:3000/jobs')
+    const response = await this.fetchData<any>('http://localhost:3000/JobOffer')
     return response
   }
 }
