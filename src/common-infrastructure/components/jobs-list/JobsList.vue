cd32ad8fed2dd4929eab69e69e379b97e19aacc7
dap <dalfonzo18@gmail.com>
2021-07-28 14:17:01 -0400

    [feat] Agrega un filtro para mostrar ofertas y trabajos
    En este commit se agrega la posibilidad de filtrar
    las ofertas de las que son trabajo y las que no.
    
    La finalidad es facilitar la UX del usuario al
    momento de interactuar con la aplicación.

--- src/common-infrastructure/components/jobs-list/JobsList.vue
+++ src/common-infrastructure/components/jobs-list/JobsList.vue
@@ -1,6 +1,11 @@
 <template>
+  <button class="jobs-list_btn" @click.prevent="toggle" label="Toggle">
+    <i type="button" class="pi pi-filter jobs-list_icon" />
+    Filtrar
+    <Menu id="overlay_menu" ref="menu" :model="items" :popup="true" />
+  </button>
   <ul class="jobs-list">
-    <li v-for="(job, index) in jobOffers" :key="index">
+    <li v-for="(job, index) in jobsData" :key="index">
       <JobsCard
         :title="job.title"
         :specialRequirements="job.specialRequirements"
@@ -13,18 +18,59 @@
   </ul>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue'
 import JobsCard from '../jobs-card/JobsCard.vue'
+import Menu from 'primevue/menu'
+import { ref } from 'vue'
 
 export default defineComponent({
   computed: {
-    jobOffers() {
-      return this.$store.getters.getAllJobOffers
+    jobsData() {
+      const jobs = this.$store.getters.getAllJobOffers
+      switch (this.filter) {
+        case 'offers':
+          return jobs.filter((job) => job.status === 0)
+        case 'jobs':
+          return jobs.filter((job) => job.status !== 0)
+        default:
+          return jobs
+      }
     },
+  },
+  setup(props, context) {
+    const filter = ref('all')
+    const menu = ref()
+    const items = ref([
+      {
+        label: 'Todo',
+        command: () => {
+          filter.value = 'all'
+        },
+      },
+      {
+        label: 'Ver ofertas',
+        command: () => {
+          filter.value = 'offers'
+        },
+      },
+      {
+        label: 'Ver Trabajos',
+        command: () => {
+          filter.value = 'jobs'
+        },
+      },
+    ])
+
+    const toggle = (event) => {
+      menu.value.toggle(event)
+    }
+
+    return { items, menu, toggle, filter }
   },
   components: {
     JobsCard,
+    Menu,
   },
 })
 </script>
