3bd9d48a81589441504a2ff870f66e4312af52fd
dap <dalfonzo18@gmail.com>
2021-07-30 19:57:37 -0400

    [UI] Agrega nueva vista de detalles
    Cambios de diseño y mejoras visuales
    en los detalles del trabajo

--- src/common-infrastructure/components/jobs-details/JobsDetails.vue
+++ src/common-infrastructure/components/jobs-details/JobsDetails.vue
@@ -1,75 +1,156 @@
 <template>
-  <main class="jobs-details" v-if="details.title">
-    <h1 class="jobs-details_title">{{ details.title }}</h1>
-    <div class="jobs-details_section">
-      <i class="pi pi-calendar jobs-details_section_icon"></i>
-      <p class="jobs-details_section_sub-title">Fecha final:</p>
-      <p>{{ formatDate(details.deadline) }}</p>
+  <header class="jobs-details_header" v-if="details.title">
+    <img :src="image" alt="header-bg" class="jobs-details_header_img" />
+    <div class="jobs-details_header_content">
+      <h1 class="jobs-details_header_title">{{ details.title }}</h1>
+      <div class="jobs-details_header_content--details">
+        <ul class="jobs-details_header_list" v-if="details.skills">
+          <li
+            v-for="skill in details.skills"
+            :key="skill"
+            class="jobs-details_header_list_item"
+          >
+            {{ skill.name }}
+          </li>
+        </ul>
+        <p v-if="details.deadline">
+          <i class="pi pi-clock jobs-details_header_content_icon" />
+          {{ formatDate(details.deadline) }}
+        </p>
+      </div>
     </div>
-    <div class="jobs-details_section">
-      <i class="pi pi-map-marker jobs-details_section_icon"></i>
-      <p class="jobs-details_section_sub-title">Ubicación:</p>
-      <p>{{ details.location.state }}, {{ details.location.city }}</p>
-    </div>
-    <div class="jobs-details_section">
-      <i class="pi pi-money-bill jobs-details_section_icon"></i>
-      <p class="jobs-details_section_sub-title">Salario:</p>
-      <p>{{ details.hourlyRate }} USD</p>
-    </div>
-    <div class="jobs-details_section">
-      <p class="jobs-details_section_sub-title">Duración:</p>
-      <p>{{ details.duration }} horas</p>
-    </div>
-    <div class="jobs-details_section">
-      <p class="jobs-details_section_sub-title">Habilidades requeridas:</p>
-      <ul class="jobs-details_section_list" v-if="details.skills">
-        <li
-          v-for="skill in details.skills"
-          :key="skill"
-          class="jobs-details_section_list_item"
-        >
-          {{ skill.name }}
-        </li>
-      </ul>
-    </div>
-    <div class="jobs-details_section">
-      <p class="jobs-details_section_sub-title">Horarios:</p>
-      <ul class="jobs-details_section_list">
-        <li
-          v-for="schedule in details.schedules"
-          :key="schedule"
-          class="jobs-details_section_list_item"
-        >
+  </header>
+  <div class="jobs-details_info-container">
+    <section class="jobs-details__job" v-if="details.title">
+      <p class="jobs-details_section-title">Información de oferta</p>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Ubicación:</p>
+        <p class="jobs-details_section_text">
+          {{ details.location.state }}, {{ details.location.city }}
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Salario:</p>
+        <p class="jobs-details_section_text">{{ details.hourlyRate }} USD</p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Duración:</p>
+        <p class="jobs-details_section_text">{{ details.duration }} horas</p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Horarios:</p>
+        <table class="jobs-details_section_table">
+          <thead>
+            <tr>
+              <th class="jobs-details_section_cell">Fecha Inicio</th>
+              <th class="jobs-details_section_cell">Hora Inicio</th>
+              <th class="jobs-details_section_cell">Fecha Fin</th>
+              <th class="jobs-details_section_cell">Hora Fin</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="schedule in details.schedules" :key="schedule">
+              <td class="jobs-details_section_row">
+                {{ formatDate(schedule.init_date) }}
+              </td>
+              <td class="jobs-details_section_row">
+                {{ formatDate(schedule.init_date, true) }}
+              </td>
+              <td class="jobs-details_section_row">
+                {{ formatDate(schedule.end_date) }}
+              </td>
+              <td class="jobs-details_section_row">
+                {{ formatDate(schedule.end_date, true) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Requerimientos especiales:</p>
+        <p class="jobs-details_section_text">
+          {{ details.specialRequirements }}
+        </p>
+      </div>
+    </section>
+    <section
+      class="jobs-details__employee"
+      v-if="details?.employee?.first_name"
+    >
+      <p class="jobs-details_section-title">Información de empleado</p>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Nombre:</p>
+        <p class="jobs-details_section_text">
           {{
-            formatDate(schedule.init_date, true) +
-            ' - ' +
-            formatDate(schedule.end_date, true)
+            `${details.employee.first_name} ${details.employee.middle_name} ${details.employee.last_name}`
           }}
-        </li>
-      </ul>
-    </div>
-    <div class="jobs-details_section">
-      <p class="jobs-details_section_sub-title">Requerimientos especiales:</p>
-      <p>{{ details.specialRequirements }}</p>
-    </div>
-  </main>
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Fecha de nacimiento:</p>
+        <p class="jobs-details_section_text">
+          {{ formatDate(new Date(details.employee.birth_date)) }}
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">SSN:</p>
+        <p class="jobs-details_section_text">
+          {{ details.employee.ssn }}
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Teléfono:</p>
+        <p class="jobs-details_section_text">
+          {{ details.employee.phone_number }}
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Dirección:</p>
+        <p class="jobs-details_section_text">
+          {{
+            `${details.employee.address.state}, ${details.employee.address.city} ${details.employee.address.street1}`
+          }}
+        </p>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Habilidades:</p>
+        <ul class="jobs-details_header_list" v-if="details.employee.skills">
+          <li
+            v-for="skill in details.employee.skills"
+            :key="skill"
+            class="jobs-details_header_list_item"
+          >
+            {{ skill.name }}
+          </li>
+        </ul>
+      </div>
+      <div class="jobs-details_section">
+        <p class="jobs-details_section_sub-title">Valoración:</p>
+        <p class="jobs-details_section_text">
+          <i class="pi pi-star jobs-details_header_content_icon" />
+          {{ details.employee.rating }}
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { RouteParams } from 'vue-router'
-import { formatDateWithTime, formatDate } from '../../shared'
+import { formatDate, formatTime } from '../../shared'
 
 export default defineComponent({
   data(): RouteParams {
     return {
       jobId: this.$route.params.id,
+      image: `https://picsum.photos/1200/200?random=${new Date()}`,
     }
   },
   methods: {
     formatDate(date: string, time?: boolean) {
       if (time) {
-        return formatDateWithTime(new Date(date))
+        return formatTime(new Date(date))
       }
       return formatDate(new Date(date), '/')
     },
